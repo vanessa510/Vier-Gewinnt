66ee0e0110290db94db6dcaca8188fb4f76bf596
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-05-14 14:42:14 +0200

    implement all tests for set and player.

--- src/test/scala/de/htwg/se/connect4/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/connect4/model/PlayerSpec.scala
@@ -1,5 +1,39 @@
 package de.htwg.se.connect4.model
 
-class PlayerSpec {
+import org.scalatest.{Matchers, WordSpec}
+
+class PlayerSpec extends WordSpec with Matchers {
+  "A player represents a participant. A player" when {
+
+    "is initialized" should {
+      val player = Player("Player1", "red")
+
+      "has 21 pieces at beginning" in {
+        player.piecesLeft should be(21)
+      }
+    }
+
+    "has a name and it" should {
+      val player = Player("Player1", "red")
+
+      "be printed" in {
+        player.toString
+      }
+    }
+
+    "is playing" should {
+      val player = Player("Player1", "red")
+
+      "set pieces and reduce amount of left pieces" in {
+        val newPlayer = player.setPiece()
+
+        newPlayer.piecesLeft should be(20)
+
+        val secondRoundPlayer = newPlayer.setPiece()
+        secondRoundPlayer.piecesLeft should be(19)
+
+      }
+    }
+  }
 
 }
--- src/test/scala/de/htwg/se/connect4/model/SetSpec.scala
+++ src/test/scala/de/htwg/se/connect4/model/SetSpec.scala
@@ -1,5 +1,29 @@
 package de.htwg.se.connect4.model
 
-class SetSpec {
+import org.scalatest.{Matchers, WordSpec}
+
+class SetSpec extends WordSpec with Matchers {
+
+  "A set represents a row or column of cells" when {
+
+    "a set with empty cells is created" should {
+      val set = Set(Vector[Cell](Cell(false), Cell(false)))
+
+      "with empty cells" in {
+        set.cells(0) should be(Cell(false))
+        set.cells(1) should be(Cell(false))
+      }
+    }
+
+    "a set with one cell set is created" should {
+      val set = Set(Vector[Cell](Cell(true, Some("red")), Cell(false)))
+
+      "contain a cell with value" in {
+        set.cells(0).isSet should be(true)
+        set.cells(1).isSet should be(false)
+        set.cells(0).color should be(Some("red"))
+      }
+    }
+  }
 
 }
