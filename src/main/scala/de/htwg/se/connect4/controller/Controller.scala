35b99f7917f94c2672c7232bc52c261231d8202c
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-06-04 12:20:32 +0200

    check if pieces are left and player win.

--- src/main/scala/de/htwg/se/connect4/controller/Controller.scala
+++ src/main/scala/de/htwg/se/connect4/controller/Controller.scala
@@ -19,11 +19,29 @@ class Controller(var board: Board, var players: List[Player]) extends Observable
       board = board.set(row, col, players(currentPlayerIndex).color)
       players = players.updated(currentPlayerIndex, players(currentPlayerIndex).setPiece())
 
+      if (playerWin(row, col)) return "Congratulations " + players(currentPlayerIndex).playerName + "You win."
+
+      if (playersHaveNoPiecesLeft) return "Game over. No pieces left. Press 'n' to start a new game."
+
       currentPlayerIndex = getNextPlayerIndex
       notifyObservers
       getPlayerDemandString
 
     }
+  }
+
+  def playerWin(row: Int, col: Int): Boolean = {
+    val matchInCols =  board.checkCols(col, players(currentPlayerIndex).color)
+    val matchInRows = board.checkRow(row, players(currentPlayerIndex).color)
+
+    matchInCols || matchInRows
+  }
+
+  def playersHaveNoPiecesLeft: Boolean = {
+    if (players.head.piecesLeft == 0 && players(1).piecesLeft == 0) return true
+
+    false
+
   }
 
   def getNextPlayerIndex: Int = if (currentPlayerIndex == 0) 1 else 0
