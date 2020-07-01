ad01af59798067831851b7845c51209edf79569c
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-07-01 10:18:45 +0200

    add Board Interface.

--- src/main/scala/de/htwg/se/connect4/model/boardComponent/BoardInterface.scala
+++ src/main/scala/de/htwg/se/connect4/model/boardComponent/BoardInterface.scala
@@ -1,5 +1,19 @@
 package de.htwg.se.connect4.model.boardComponent
 
+import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.Color.Color
+import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.{Board, Cell, Color, Matrix, Set}
+
 trait BoardInterface {
+
+  def sizeOfRows: Int
+  def sizeOfCols: Int
+  def cell(row: Int, col: Int): Cell
+  def col(col: Int): Set
+  def row(row: Int): Set
+  def set(row: Int, col: Int, color: Color.Value, isSet: Boolean): Board
+  def checkRow(row: Int, color: Color): Boolean
+  def checkCols(col: Int, color: Color): Boolean
+  def checkDiagonal(row: Int, col: Int, playerColor: Color): Boolean
+  def getBoardAsString(matrix: Matrix[Cell]): String
 
 }
