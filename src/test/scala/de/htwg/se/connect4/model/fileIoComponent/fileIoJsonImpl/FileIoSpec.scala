d43495a1ef6db1561782045e5878b57a354afe37
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-07-08 13:33:00 +0200

    add test for json file io.

---
+++ src/test/scala/de/htwg/se/connect4/model/fileIoComponent/fileIoJsonImpl/FileIoSpec.scala
@@ -0,0 +1,31 @@
+package de.htwg.se.connect4.model.fileIoComponent.fileIoJsonImpl
+
+import de.htwg.se.connect4.controller.controllerComponent.controllerBaseImpl.{Controller, State}
+import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.{Board, Color}
+import de.htwg.se.connect4.model.playerComponent.Player
+import org.scalatest.{Matchers, WordSpec}
+
+class FileIoSpec extends WordSpec with Matchers {
+
+  "A File IO" when {
+
+    "loading" should {
+      var board = new Board(2, 4, false)
+      val players: List[Player] = Player("test1", Color.RED, 1) :: Player("test2", Color.YELLOW, 0) :: Nil
+      val controller = new Controller(board, players)
+
+      "get correct board and state" in {
+        controller.fileIo.save(board, new State(controller.currentPlayerIndex, players, controller.state.toString()))
+        val oldBoard = board.copy()
+        val (newBoard, state) = controller.fileIo.load
+
+        newBoard.sizeOfCols shouldBe 7
+
+
+
+
+      }
+    }
+  }
+
+}
