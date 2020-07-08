8b0858df9937ec366e6e7232f89ba98e1a1fbf25
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2020-07-08 13:39:44 +0200

    add xml test.

--- src/test/scala/de/htwg/se/connect4/model/fileIoComponent/fileIoXmlImpl/FileIoSpec.scala
+++ src/test/scala/de/htwg/se/connect4/model/fileIoComponent/fileIoXmlImpl/FileIoSpec.scala
@@ -1,4 +1,4 @@
-package de.htwg.se.connect4.model.fileIoComponent.fileIoJsonImpl
+package de.htwg.se.connect4.model.fileIoComponent.fileIoXmlImpl
 
 import de.htwg.se.connect4.controller.controllerComponent.controllerBaseImpl.{Controller, State}
 import de.htwg.se.connect4.model.boardComponent.boardBaseImpl.{Board, Color}
@@ -9,20 +9,20 @@ class FileIoSpec extends WordSpec with Matchers {
 
   "A File IO" when {
 
-    "loading" should {
+    "loaded" should {
       var board = new Board(2, 4, false)
       val players: List[Player] = Player("test1", Color.RED, 1) :: Player("test2", Color.YELLOW, 0) :: Nil
       val controller = new Controller(board, players)
 
       "get correct board and state" in {
-        controller.fileIo.save(board, new State(controller.currentPlayerIndex, players, controller.state.toString()))
+
+        val fileIo = new FileIO
+        fileIo.save(board, new State(controller.currentPlayerIndex, players, controller.state.toString()))
         val oldBoard = board.copy()
-        val (newBoard, state) = controller.fileIo.load
+        val (newBoard, state) = fileIo.load
 
         newBoard.sizeOfCols shouldBe 7
-
       }
     }
   }
-
 }
